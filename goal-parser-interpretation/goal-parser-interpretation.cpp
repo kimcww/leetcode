173ccf704fb20a53f08f96c26e92c4248aa797c7
kimchangwoo <42766429+kimcww@users.noreply.github.com>
2021-01-24 15:08:36 +0900

    Time: 0 ms (100.00%), Space: 6.1 MB (97.42%) - LeetHub

---
+++ goal-parser-interpretation/goal-parser-interpretation.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string ret;
+        for (int i = 0 ; i < command.size(); i++)
+        {
+            if (command[i] == '(')
+            {
+                if (command[i + 1] == ')')
+                    ret += 'o';
+                continue;
+            }
+            else if(command[i] == ')')
+                continue;
+            
+            ret += command[i];
+        }
+        return ret;
+    }
+};
