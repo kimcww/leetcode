f64dea8c58dbfc6fefdbf207a7f3c93fcecdbe79
kimchangwoo <42766429+kimcww@users.noreply.github.com>
2021-08-22 20:31:51 +0900

    Update Q9.java

--- java/Q9/Q9.java
+++ java/Q9/Q9.java
@@ -3,7 +3,7 @@ import java.util.*
 public class Q9 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        String data = "in.next()";
+        String data = in.next();
         String result = "";
         int res = 0;
         for(Character a : data.toCharArray())
