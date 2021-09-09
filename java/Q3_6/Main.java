b8ec2c3b9635b169e2d5a36cbc27c38cfa59fcdb
Kimcw <kcw204@gmail.com>
2021-09-09 19:53:08 +0900

    desc 추가

--- java/Q3_6/Main.java
+++ java/Q3_6/Main.java
@@ -1,5 +1,8 @@
 import java.util.*;
-
+//
+//알고리즘의 요지 , rt 가 == 0 이면 0의 count 를 +1 하고 k 값보다 count 가 큰지 안큰지 확인한다
+//count 가 > k보다 크면 lt 가 ==0 일때까지 따라오면서 0 일때 -1 한다 현재 rt-lt+1 값이 1의 연속 길이
+//
 public class Main {
     public static void main(String[] args)
     {
@@ -23,7 +26,7 @@ public class Main {
                     count--;
                 lt++;
             }
-            result= Math.max(result, rt-lt+1);
+            result= Math.max(result, rt-lt+1); // rt - lt +1 = 1의 길이
         }
         System.out.print(result);
     }
